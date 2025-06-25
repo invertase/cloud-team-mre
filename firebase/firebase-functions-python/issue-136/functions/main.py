09faf721b53f4b5488cb5a3ba3c942ded3b9c446
Corie Watson <watson.corie@gmail.com>
2025-06-25 15:43:50 +0100

    init

---
+++ firebase/firebase-functions-python/issue-136/functions/main.py
@@ -0,0 +1,24 @@
+from firebase_functions import firestore_fn
+from firebase_admin import initialize_app
+
+initialize_app()
+
+@firestore_fn.on_document_created(document="messages/{pushId}")
+def makeuppercase(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -> None:
+    """Listens for new documents to be added to /messages. If the document has
+    an "original" field, creates an "uppercase" field containg the contents of
+    "original" in upper case."""
+
+    # Get the value of "original" if it exists.
+    if event.data is None:
+        return
+    try:
+        original = event.data.get("original")
+    except KeyError:
+        # No "original" field, so do nothing.
+        return
+
+    # Set the "uppercase" field.
+    print(f"Uppercasing {event.params['pushId']}: {original}")
+    upper = original.upper()
+    event.data.reference.update({"uppercase": upper})
