4a2e6998fc59141d2d8010b2b0a2b8b1dea28bac
HassanBahati <mukisabahati@gmail.com>
2025-12-16 19:10:06 +0300

    init

--- functions/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { onRequest } from "firebase-functions/https";
-
-// The module that would not be included in the Cloud Run container.
-// import { value } from "./feature.local/somethingModule";
-const value = "some value";
-
-export const helloFailingInitialDeploy = onRequest((request, response) => {
-  response.send(`Hello from Firebase! The value is: ${value}`);
-});
