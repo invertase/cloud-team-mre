cecf155d58cc4ba836e076dd177fb2174afe3032
HassanBahati <mukisabahati@gmail.com>
2025-12-18 16:20:00 +0300

    chore: add basic hello world v2 HTTP function

--- issue-1734/functions/index.ts
+++ issue-1734/functions/index.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase-admin/app";
 import { onSchedule } from "firebase-functions/v2/scheduler";
+import { onRequest } from "firebase-functions/v2/https";
 
 initializeApp();
 
@@ -16,3 +17,18 @@ export const workingScheduler = onSchedule(
     console.log("This function works correctly");
   }
 );
+
+export const schedulerWithRegion = onSchedule(
+  {
+    schedule: "every 1 minutes",
+    timeZone: "America/Los_Angeles",
+    region: "us-central1",
+  },
+  async () => {
+    console.log("This function works correctly");
+  }
+);
+
+export const helloWorldV2Issue1734 = onRequest((req, res) => {
+  res.send("Hello from v2, issue 1734");
+});
